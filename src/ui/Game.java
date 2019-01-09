cc08685d977c0589a80332bde192aea6f0cb46f8
chriswolfdesign <chriswolfdesign@gmail.com>
2019-01-09 10:56:57 -0500

    "Added JavaDoc to Game.java and Window.java"

--- src/ui/Game.java
+++ src/ui/Game.java
@@ -1,5 +1,15 @@
 package ui;
 
+/**
+ * Game.java
+ *
+ * The entrypoint for our Snake game.
+ *
+ * @author Chris Wolf
+ * @version 1.0.0 (January 9, 2019)
+ *
+ * chriswolfdesign@gmail.com
+ */
 public class Game {
     public static void main(String[] args) {
         Window window = new Window();
--- src/ui/Window.java
+++ src/ui/Window.java
@@ -3,6 +3,17 @@ package ui;
 import javax.swing.*;
 import java.awt.*;
 
+/**
+ * Window.java
+ *
+ * The class that will create and maintain the window in which the game is run.
+ *
+ * @author Chris Wolf
+ * @version 1.0.0 (January 9, 2019)
+ *
+ * chriswolfdesign@gmail.com
+ */
+
 public class Window extends JFrame {
     public Window() {
         super("Snake");
