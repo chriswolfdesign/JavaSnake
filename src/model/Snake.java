a746f10c74b1861af8c615158b4a761f693e9f31
chriswolfdesign <chriswolfdesign@gmail.com>
2019-01-10 13:21:43 -0500

    "Implemented Snakes's movement methods"

--- src/model/Snake.java
+++ src/model/Snake.java
@@ -15,6 +15,8 @@ import java.util.ArrayList;
 
 public class Snake {
     private ArrayList<SnakePiece> pieces;
+    private int dx;
+    private int dy;
 
     public Snake(int x, int y) {
         pieces = new ArrayList<>();
@@ -22,6 +24,10 @@ public class Snake {
         for (int i = 0; i < 3; i++) {
             pieces.add(new SnakePiece(x, y + SnakePiece.getPieceSize() * i));
         }
+
+        // Begin with the snake moving up
+        dx = 0;
+        dy = -1 * SnakePiece.getPieceSize();
     }
 
     /**
@@ -43,10 +49,60 @@ public class Snake {
     }
 
     /**
-     * Adds a piece to the end of our snake (placing it below the most recent piece)
+     * Adds a piece to the end of our snake
+     * (placing it below the most recent piece)
      */
     public void addPiece() {
         this.pieces.add(new SnakePiece(pieces.get(this.size() - 1).getX(),
-                pieces.get(this.size() - 1).getY() + SnakePiece.getPieceSize()));
+                pieces.get(this.size() - 1).getY() +
+                        SnakePiece.getPieceSize()));
+    }
+
+    /**
+     * Causes the Snake to move one "pixel" based on the current
+     * dx and dy values
+     */
+    public void move() {
+        // Put all pieces of the snake where the previous piece was
+        for (int i = this.size() - 1; i > 0; i--) {
+            this.pieces.get(i).setX(this.pieces.get(i - 1).getX());
+            this.pieces.get(i).setY(this.pieces.get(i - 1).getY());
+        }
+
+        // Move the head of the snake based on the current dx and dy values
+        this.pieces.get(0).setX(this.pieces.get(0).getX() + this.dx);
+        this.pieces.get(0).setY(this.pieces.get(0).getY() + this.dy);
+    }
+
+    /**
+     * Sets the dx and dy values such that the snake will be moving up.
+     */
+    public void goUp() {
+        this.dx = 0;
+        this.dy = -1 * SnakePiece.getPieceSize();
+    }
+
+    /**
+     * Sets the dx and dy values such that the snake will be moving down.
+     */
+    public void goDown() {
+        this.dx = 0;
+        this.dy = SnakePiece.getPieceSize();
+    }
+
+    /**
+     * Sets the dx and dy values such that the snake will be moving right.
+     */
+    public void goRight() {
+        this.dx = SnakePiece.getPieceSize();
+        this.dy = 0;
+    }
+
+    /**
+     * Sets the dx and dy values such that the snake will be moving left.
+     */
+    public void goLeft() {
+        this.dx = -1 * SnakePiece.getPieceSize();
+        this.dy = 0;
     }
 }
--- src/test/SnakeTest.java
+++ src/test/SnakeTest.java
@@ -25,4 +25,53 @@ public class SnakeTest {
         s.addPiece();
         assertEquals(4, s.size(), 0.0);
     }
+
+    @Test
+    public void testMove() {
+        assertEquals(100, s.getPieces().get(0).getX(), 0.0);
+        assertEquals(200, s.getPieces().get(0).getY(), 0.0);
+        s.move();
+        assertEquals(100, s.getPieces().get(0).getX(), 0.0);
+        assertEquals(190, s.getPieces().get(0).getY(), 0.0);
+    }
+
+    @Test
+    public void testGoUp() {
+        assertEquals(100, s.getPieces().get(0).getX(), 0.0);
+        assertEquals(200, s.getPieces().get(0).getY(), 0.0);
+        s.goUp();
+        s.move();
+        assertEquals(100, s.getPieces().get(0).getX(), 0.0);
+        assertEquals(190, s.getPieces().get(0).getY(), 0.0);
+    }
+
+    @Test
+    public void testGoDown() {
+        assertEquals(100, s.getPieces().get(0).getX(), 0.0);
+        assertEquals(200, s.getPieces().get(0).getY(), 0.0);
+        s.goDown();
+        s.move();
+        assertEquals(100, s.getPieces().get(0).getX(), 0.0);
+        assertEquals(210, s.getPieces().get(0).getY(), 0.0);
+    }
+
+    @Test
+    public void testGoRight() {
+        assertEquals(100, s.getPieces().get(0).getX(), 0.0);
+        assertEquals(200, s.getPieces().get(0).getY(), 0.0);
+        s.goRight();
+        s.move();
+        assertEquals(110, s.getPieces().get(0).getX(), 0.0);
+        assertEquals(200, s.getPieces().get(0).getY(), 0.0);
+    }
+
+    @Test
+    public void testGoLeft() {
+        assertEquals(100, s.getPieces().get(0).getX(), 0.0);
+        assertEquals(200, s.getPieces().get(0).getY(), 0.0);
+        s.goLeft();
+        s.move();
+        assertEquals(90, s.getPieces().get(0).getX(), 0.0);
+        assertEquals(200, s.getPieces().get(0).getY(), 0.0);
+    }
 }
